5b68405ab922eec0caa334ca003ff59cbc870c7a
GharbiHaithem <gharbi.haythem1988@gmail.com>
2025-10-11 22:20:58 +0200

    Remove email controller containing secret

--- routers/email.router.js
+++ routers/email.router.js
@@ -1,24 +1,29 @@
-// backend/routes/email.route.js
 const express = require('express');
 const router = express.Router();
-const nodemailer = require('nodemailer')
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'noreply.frogstore@gmail.com', // compte dédié
-    pass: 'fgwrxppsrthxllxs',            // mot de passe d’application
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_PASS,
   },
 });
+
 router.post('/send-pdf-email', async (req, res) => {
   const { to, pdfUrl, subject } = req.body;
 
   try {
     await transporter.sendMail({
-      from: 'noreply.frogstore@gmail.com',
+      from: process.env.GMAIL_USER,
       to,
       subject,
-      html: `<p>Bonjour,</p><p>Voici votre facture :</p><a href="${pdfUrl}">${pdfUrl}</a>`,
+      html: `
+        <p>Bonjour,</p>
+        <p>Voici votre facture :</p>
+        <a href="${pdfUrl}" target="_blank">${pdfUrl}</a>
+      `,
     });
 
     res.json({ success: true });
@@ -27,4 +32,5 @@ router.post('/send-pdf-email', async (req, res) => {
     res.status(500).json({ success: false, error: error.message });
   }
 });
+
 module.exports = router;
