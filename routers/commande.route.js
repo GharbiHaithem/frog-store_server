14d327a0dcfbea5d8c547c43cf05fb8c08b1ffe7
GharbiHaithem <gharbi.haythem1988@gmail.com>
2025-10-17 20:15:40 +0200

    Remove sensitive files

--- routers/commande.route.js
+++ routers/commande.route.js
@@ -1,9 +1,13 @@
-const {createCommande,getCommande,getCommandeFromUser,commandeById} =require('../controllers/commandeCtrlr')
+const {createCommande,getCommande,getCommandeFromUser,commandeById,gettAllCommande} =require('../controllers/commandeCtrlr')
 const express= require('express')
 const router = express.Router()
 
 router.post('/create-commande',createCommande)
+
+
+router.get('/comande/all',gettAllCommande)
 router.get('/getcommande/:commandeid',commandeById)
+
 router.get('/commande/user/:userid',getCommandeFromUser)
 router.get('/commande/:userid',getCommande)
 
